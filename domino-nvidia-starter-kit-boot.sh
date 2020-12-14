082288ef73a472fd2698b2ff63e6bdb7a8d1694c
Nicholas Jablonski <nick.jablonski@dominodatalab.com>
2020-12-14 15:29:38 -0500

    Update domino-nvidia-starter-kit-boot.sh

--- domino-nvidia-starter-kit-boot.sh
+++ domino-nvidia-starter-kit-boot.sh
@@ -17,7 +17,7 @@ echo "Provisioning MIG resources..."
 sudo nvidia-smi mig -cgi 19,19,19,19,19,19,19 -C
 
 echo "Running the device plugin and feature discovery playbooks to restart the Kubernetes GPU controllers and detect the new virtual devices..."
-cd deepops
+cd ../deepops
 sudo -u ubuntu ansible-playbook playbooks/k8s-cluster/nvidia-k8s-gpu-device-plugin.yml playbooks/k8s-cluster/nvidia-k8s-gpu-feature-discovery.yml | tee /dev/tty | grep -q "failed=0"
 if [ $? -ne 0 ] ; then
         echo "Issue running playbook"
